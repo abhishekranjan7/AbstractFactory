3252582756d90f767256e9b866f40407e526ec44
abhishekranjan7 <mnnit4abhishek@gmail.com>
2021-09-02 18:30:52 +0530

    Abstract Factory Code

---
+++ OSFactory.java
@@ -0,0 +1,9 @@
+  
+package com.techprimers.designpatterns.abstractfactory;
+
+import com.techprimers.designpatterns.factory.PhoneType;
+
+public interface OSFactory {
+
+    Phone create(ManufacturerType manufacturerType);
+}
